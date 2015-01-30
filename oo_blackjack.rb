d3571e996f2d00396d44e2d8ca909d8b055be562
digitalG2 <jblynn2@gmail.com>
2015-01-29 23:11:52 -0700

    Adding mor functionality to classes.

--- oo_blackjack.rb
+++ oo_blackjack.rb
@@ -1,35 +1,87 @@
+require "pry"
+
 class Player
-  attr_accessor :name
+  attr_accessor :name, :hand, :score
 
   def initialize(name)
     @name = name
+    @hand = []
   end
   
-  def hand
+  def display_hand
+    puts "#{name}'s Hand:" 
+    @hand.each { |value| puts "#{value[0]} of #{value[1]}" }
+    puts
   end
-  
-  def score
+
+  def calculate_score
+    @score = 0
+    @hand.each do |value| 
+      case value[0]
+      when 2..10
+        @score += value[0]
+      when 'Jack', 'King', 'Queen'
+        @score += 10
+      when 'Ace'
+        @score += 11
+      end
+    end
+    @hand.select{ |card| card[0] == 'Ace' }.count.times do
+      @score -= 10 if @score > 21
+    end
+    @score
   end
+
+  def say_score
+    puts "#{@name}'s Score: #{calculate_score}"
+  end
+
+  def show_info
+    system 'clear'
+    display_hand
+    say_score
+    puts
+  end
+
 end
 
 class HumanPlayer < Player
   attr_accessor :bet, :purse
 
   def initialize(name)
+    super
     @purse = 500
-    @name = name
   end
  
   def place_bet
     puts "You have $#{purse} #{name}."
     puts 'How much would you like to wager this round?'
     @bet = gets.chomp.to_i
+    system 'clear'
     puts "You have bet $#{bet}."
+    puts
+  end
+
+  def hit_or_stay(deck, player, computer)
+    loop do
+      begin
+        puts 'Would you like to stay or hit? (Enter S or H)'
+        hit_or_stay = gets.chomp.upcase 
+        puts "Invalid entry. Please try again" if !['S', 'H'].include?(hit_or_stay)
+      end until hit_or_stay == 'S' || hit_or_stay == 'H'
+      deck.deal(player.hand) if hit_or_stay == 'H'
+      system 'clear'
+      player.show_info
+      break if hit_or_stay == 'S' 
+    end
   end
 
 end
 
 class ComputerPlayer < Player
+  def initialize(name)
+    super
+  end
 end
 
 class Card
@@ -43,26 +95,33 @@ end
 
 class Deck < Card
   @@num_decks = 0
+  @@game_deck = []
   
   def initialize
     @@num_decks += 1
+    @@game_deck = add_deck
+    @name = 'deck_' + @@num_decks.to_s
   end
 
-  def new_deck
-    game_deck = VALUES.product(SUITS)
-    game_deck
+  def add_deck
+    @@game_deck = @@game_deck + VALUES.product(SUITS).shuffle
+    @@game_deck
   end
+
+  def deal(hand)
+    if @@game_deck.length > 0
+      hand.push @@game_deck.pop
+    else
+      add_deck
+      deal(hand)
+    end
+  end
+
 
 end
 
 class Game
   @@game_count = 0
-
-  def play_again
-    puts "Would you like to play again?"
-    response = gets.chomp
-    response
-  end
 
   def initialize
     system 'clear'
@@ -70,16 +129,34 @@ class Game
       puts 'Welcome to Blackjack. What\'s your name?'
       name = gets.chomp
       player1 = HumanPlayer.new(name)
+      computer = ComputerPlayer.new('Computer')
     end
     @@game_count += 1
-    play(player1)
+    play(player1, computer)
   end
 
-  def play(player1)
+  def play_again
+    puts "Would you like to play again?"
+    response = gets.chomp
+    response
+  end
+
+  
+
+  def play(player1, computer)
+    system 'clear'
     player1.place_bet
-    game_deck = Deck.new_deck
-    puts game_deck
-    play(player1) if play_again == 'Y'
+    game_deck = Deck.new
+    game_deck.deal(player1.hand)
+    game_deck.deal(computer.hand)
+    computer.display_hand
+    game_deck.deal(player1.hand)
+    game_deck.deal(computer.hand)
+    player1.show_info
+    player1.hit_or_stay(game_deck, player1, computer)
+
+    
+    play(player1, computer) if play_again == 'Y'
   end
 end
 
